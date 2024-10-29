e16ba5b3c30723e548c262d94e86be7b7b91e2aa
Sanjoy Saha <sanjoysaha0524@gmail.com>
2024-10-29 16:47:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// gsap.to(".main",{
+//   x:1000, 
+//   duration: 10,
+//   delay:4,
+//   rotate:90,
+//   backgroundColor:"blue"
+// })
+
+// const timeline = gsap.timeline();
+// timeline.to(".box1",{
+//     x:1200,
+//     rotate: 90,
+//     scale:0.5,
+//     duration:4,
+//     delay:2,
+// })
+
